ecc829b1f64c19506b35989ce1be0b53176193bf
Matiasahumada <pagosptcmatias@gmail.com>
2024-03-16 14:14:41 -0300

    primer commit iniciando repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'react-bootstrap';
+import NavBar from './components/common/Nav'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <Container>
+      <NavBar></NavBar>
+    </Container>
+  );
+}
+
+export default App;
